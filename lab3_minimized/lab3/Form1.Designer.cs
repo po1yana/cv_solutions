4cddba1b1fb83b4da3b386a0de725aa46be72576
Lapin Egor <104688678+po1yana@users.noreply.github.com>
2024-06-14 01:34:45 +0300

    commit lab3

---
+++ lab3_minimized/lab3/Form1.Designer.cs
@@ -0,0 +1,296 @@
+﻿namespace lab3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.imageBox1 = new Emgu.CV.UI.ImageBox();
+            this.imageBox2 = new Emgu.CV.UI.ImageBox();
+            this.button_open_image = new System.Windows.Forms.Button();
+            this.numericUpDown_width = new System.Windows.Forms.NumericUpDown();
+            this.button_scale = new System.Windows.Forms.Button();
+            this.numericUpDown_height = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button_process = new System.Windows.Forms.Button();
+            this.textBox_shift = new System.Windows.Forms.TextBox();
+            this.textBox_angle = new System.Windows.Forms.TextBox();
+            this.textBox_centerX = new System.Windows.Forms.TextBox();
+            this.textBox_centerY = new System.Windows.Forms.TextBox();
+            this.button_process_image = new System.Windows.Forms.Button();
+            this.button_mirror = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button_projection = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown_width)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown_height)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // imageBox1
+            // 
+            this.imageBox1.Location = new System.Drawing.Point(154, 9);
+            this.imageBox1.Name = "imageBox1";
+            this.imageBox1.Size = new System.Drawing.Size(300, 263);
+            this.imageBox1.TabIndex = 2;
+            this.imageBox1.TabStop = false;
+            this.imageBox1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.imageBox1_MouseClick);
+            // 
+            // imageBox2
+            // 
+            this.imageBox2.Location = new System.Drawing.Point(460, 9);
+            this.imageBox2.Name = "imageBox2";
+            this.imageBox2.Size = new System.Drawing.Size(328, 263);
+            this.imageBox2.TabIndex = 2;
+            this.imageBox2.TabStop = false;
+            // 
+            // button_open_image
+            // 
+            this.button_open_image.Location = new System.Drawing.Point(30, 18);
+            this.button_open_image.Name = "button_open_image";
+            this.button_open_image.Size = new System.Drawing.Size(84, 23);
+            this.button_open_image.TabIndex = 3;
+            this.button_open_image.Text = "Open image";
+            this.button_open_image.UseVisualStyleBackColor = true;
+            this.button_open_image.Click += new System.EventHandler(this.button_open_image_Click);
+            // 
+            // numericUpDown_width
+            // 
+            this.numericUpDown_width.DecimalPlaces = 1;
+            this.numericUpDown_width.Increment = new decimal(new int[] {
+            5,
+            0,
+            0,
+            65536});
+            this.numericUpDown_width.Location = new System.Drawing.Point(55, 89);
+            this.numericUpDown_width.Maximum = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.numericUpDown_width.Name = "numericUpDown_width";
+            this.numericUpDown_width.Size = new System.Drawing.Size(59, 20);
+            this.numericUpDown_width.TabIndex = 4;
+            // 
+            // button_scale
+            // 
+            this.button_scale.Location = new System.Drawing.Point(30, 59);
+            this.button_scale.Name = "button_scale";
+            this.button_scale.Size = new System.Drawing.Size(84, 23);
+            this.button_scale.TabIndex = 5;
+            this.button_scale.Text = "Scaling";
+            this.button_scale.UseVisualStyleBackColor = true;
+            this.button_scale.Click += new System.EventHandler(this.button_scale_Click);
+            // 
+            // numericUpDown_height
+            // 
+            this.numericUpDown_height.DecimalPlaces = 1;
+            this.numericUpDown_height.Increment = new decimal(new int[] {
+            5,
+            0,
+            0,
+            65536});
+            this.numericUpDown_height.Location = new System.Drawing.Point(55, 115);
+            this.numericUpDown_height.Name = "numericUpDown_height";
+            this.numericUpDown_height.Size = new System.Drawing.Size(59, 20);
+            this.numericUpDown_height.TabIndex = 6;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(31, 93);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(18, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "W";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(32, 119);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(15, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "H";
+            // 
+            // button_process
+            // 
+            this.button_process.Location = new System.Drawing.Point(30, 151);
+            this.button_process.Name = "button_process";
+            this.button_process.Size = new System.Drawing.Size(84, 23);
+            this.button_process.TabIndex = 9;
+            this.button_process.Text = "Shearing";
+            this.button_process.UseVisualStyleBackColor = true;
+            this.button_process.Click += new System.EventHandler(this.button_process_Click);
+            // 
+            // textBox_shift
+            // 
+            this.textBox_shift.Location = new System.Drawing.Point(30, 180);
+            this.textBox_shift.Name = "textBox_shift";
+            this.textBox_shift.Size = new System.Drawing.Size(84, 20);
+            this.textBox_shift.TabIndex = 10;
+            // 
+            // textBox_angle
+            // 
+            this.textBox_angle.Location = new System.Drawing.Point(75, 252);
+            this.textBox_angle.Name = "textBox_angle";
+            this.textBox_angle.Size = new System.Drawing.Size(39, 20);
+            this.textBox_angle.TabIndex = 12;
+            // 
+            // textBox_centerX
+            // 
+            this.textBox_centerX.Location = new System.Drawing.Point(75, 278);
+            this.textBox_centerX.Name = "textBox_centerX";
+            this.textBox_centerX.Size = new System.Drawing.Size(39, 20);
+            this.textBox_centerX.TabIndex = 13;
+            // 
+            // textBox_centerY
+            // 
+            this.textBox_centerY.Location = new System.Drawing.Point(75, 304);
+            this.textBox_centerY.Name = "textBox_centerY";
+            this.textBox_centerY.Size = new System.Drawing.Size(40, 20);
+            this.textBox_centerY.TabIndex = 14;
+            // 
+            // button_process_image
+            // 
+            this.button_process_image.Location = new System.Drawing.Point(30, 223);
+            this.button_process_image.Name = "button_process_image";
+            this.button_process_image.Size = new System.Drawing.Size(84, 23);
+            this.button_process_image.TabIndex = 15;
+            this.button_process_image.Text = "Rotation";
+            this.button_process_image.UseVisualStyleBackColor = true;
+            this.button_process_image.Click += new System.EventHandler(this.button_process_image_Click);
+            // 
+            // button_mirror
+            // 
+            this.button_mirror.Location = new System.Drawing.Point(30, 346);
+            this.button_mirror.Name = "button_mirror";
+            this.button_mirror.Size = new System.Drawing.Size(84, 23);
+            this.button_mirror.TabIndex = 16;
+            this.button_mirror.Text = "Mirror";
+            this.button_mirror.UseVisualStyleBackColor = true;
+            this.button_mirror.Click += new System.EventHandler(this.button_mirror_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(27, 255);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(45, 13);
+            this.label3.TabIndex = 17;
+            this.label3.Text = "degrees";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(21, 281);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(47, 13);
+            this.label4.TabIndex = 18;
+            this.label4.Text = "center X";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(22, 308);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(47, 13);
+            this.label5.TabIndex = 19;
+            this.label5.Text = "center Y";
+            // 
+            // button_projection
+            // 
+            this.button_projection.Location = new System.Drawing.Point(39, 387);
+            this.button_projection.Name = "button_projection";
+            this.button_projection.Size = new System.Drawing.Size(75, 23);
+            this.button_projection.TabIndex = 20;
+            this.button_projection.Text = "focus";
+            this.button_projection.UseVisualStyleBackColor = true;
+            this.button_projection.Click += new System.EventHandler(this.button_projection_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button_projection);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button_mirror);
+            this.Controls.Add(this.button_process_image);
+            this.Controls.Add(this.textBox_centerY);
+            this.Controls.Add(this.textBox_centerX);
+            this.Controls.Add(this.textBox_angle);
+            this.Controls.Add(this.textBox_shift);
+            this.Controls.Add(this.button_process);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.numericUpDown_height);
+            this.Controls.Add(this.button_scale);
+            this.Controls.Add(this.numericUpDown_width);
+            this.Controls.Add(this.button_open_image);
+            this.Controls.Add(this.imageBox2);
+            this.Controls.Add(this.imageBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown_width)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown_height)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Emgu.CV.UI.ImageBox imageBox1;
+        private Emgu.CV.UI.ImageBox imageBox2;
+        private System.Windows.Forms.Button button_open_image;
+        private System.Windows.Forms.NumericUpDown numericUpDown_width;
+        private System.Windows.Forms.Button button_scale;
+        private System.Windows.Forms.NumericUpDown numericUpDown_height;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button_process;
+        private System.Windows.Forms.TextBox textBox_shift;
+        private System.Windows.Forms.TextBox textBox_angle;
+        private System.Windows.Forms.TextBox textBox_centerX;
+        private System.Windows.Forms.TextBox textBox_centerY;
+        private System.Windows.Forms.Button button_process_image;
+        private System.Windows.Forms.Button button_mirror;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button_projection;
+    }
+}
+
---
+++ lab3_minimized/lab3/Form1.cs
@@ -0,0 +1,331 @@
+﻿using Emgu.CV;
+using Emgu.CV.CvEnum;
+using Emgu.CV.Ocl;
+using Emgu.CV.Structure;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lab3
+{
+    public partial class Form1 : Form
+    {
+        private Image<Bgr, byte> sourceImage;
+        private Image<Bgr, byte> processedImage;
+        private PointF[] srcPoints; // Точки для исходного изображения
+        private int pointsSelected = 0;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            
+
+        }
+
+        private void button_open_image_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            var result = openFileDialog.ShowDialog(); // открытие диалога выбора файла
+            if (result == DialogResult.OK) // открытие выбранного файла
+            {
+                string fileName = openFileDialog.FileName;
+                sourceImage = new Image<Bgr, byte>(fileName);
+
+                imageBox1.Image = sourceImage;
+                srcPoints = null;
+                pointsSelected = 0;
+            }
+        }
+        // 1
+        private void button_scale_Click(object sender, EventArgs e)
+        {
+            // Проверяем, что исходное изображение загружено
+            if (sourceImage != null)
+            {
+                // Коэффициенты масштабирования
+                float sX = (float)numericUpDown_width.Value;
+                float sY = (float)numericUpDown_height.Value;
+
+                // Создание нового изображения с учетом масштабирования
+                int newWidth = (int)(sourceImage.Width * sX);
+                int newHeight = (int)(sourceImage.Height * sY);
+                var newImage = new Image<Bgr, byte>(newWidth, newHeight);
+
+                // Билинейная фильтрация
+                for (int x = 0; x < newWidth; x++)
+                {
+                    for (int y = 0; y < newHeight; y++)
+                    {
+                        // Координаты исходного изображения, округленные до ближайших нижних значений
+                        int floorX = (int)Math.Floor(x / sX);
+                        int floorY = (int)Math.Floor(y / sY);
+
+                        // Дистанция от ближайших точек исходного изображения до искомой
+                        float ratioX = x / sX - floorX;
+                        float ratioY = y / sY - floorY;
+
+                        // Координаты ближайших точек на исходном изображении
+                        int baseX = Math.Min(floorX, sourceImage.Width - 2);
+                        int baseY = Math.Min(floorY, sourceImage.Height - 2);
+
+                        // Расчет значений для билинейной фильтрации
+                        float invRatioX = 1 - ratioX;
+                        float invRatioY = 1 - ratioY;
+
+                        // Вычисление нового цвета пикселя
+                        Bgr newVal = new Bgr(
+                            (sourceImage[baseY, baseX].Blue * invRatioX + sourceImage[baseY, baseX + 1].Blue * ratioX) * invRatioY +
+                            (sourceImage[baseY + 1, baseX].Blue * invRatioX + sourceImage[baseY + 1, baseX + 1].Blue * ratioX) * ratioY,
+
+                            (sourceImage[baseY, baseX].Green * invRatioX + sourceImage[baseY, baseX + 1].Green * ratioX) * invRatioY +
+                            (sourceImage[baseY + 1, baseX].Green * invRatioX + sourceImage[baseY + 1, baseX + 1].Green * ratioX) * ratioY,
+
+                            (sourceImage[baseY, baseX].Red * invRatioX + sourceImage[baseY, baseX + 1].Red * ratioX) * invRatioY +
+                            (sourceImage[baseY + 1, baseX].Red * invRatioX + sourceImage[baseY + 1, baseX + 1].Red * ratioX) * ratioY
+                        );
+
+                        // Установка значения в новое изображение
+                        newImage[y, x] = newVal;
+                    }
+                }
+
+                // Отображение обработанного изображения во втором ImageBox
+                processedImage = newImage;
+                imageBox2.Image = processedImage;
+            }
+            else
+            {
+                MessageBox.Show("Сначала откройте изображение!");
+            }
+        }
+
+
+        //2
+        private void button_process_Click(object sender, EventArgs e)
+        {
+            if (sourceImage == null)
+            {
+                MessageBox.Show("Откройте изображение сначала.", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            if (!float.TryParse(textBox_shift.Text, out float shift))
+            {
+                MessageBox.Show("Введите корректное значение сдвига.", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            processedImage = new Image<Bgr, byte>(sourceImage.Width, sourceImage.Height);
+
+            for (int y = 0; y < sourceImage.Height; y++)
+            {
+                for (int x = 0; x < sourceImage.Width; x++)
+                {
+                    int newX = (int)(x + shift * (sourceImage.Height - y));
+                    int newY = y;
+
+                    if (newX >= 0 && newX < sourceImage.Width)
+                    {
+                        processedImage[newY, newX] = sourceImage[y, x];
+                    }
+                }
+            }
+
+            imageBox2.Image = processedImage;
+        }
+
+
+        //3
+        private void button_process_image_Click(object sender, EventArgs e)
+        {
+            if (sourceImage == null)
+            {
+                MessageBox.Show("Сначала выберите изображение");
+                return;
+            }
+
+            // Получаем параметры из формы
+            double angle;
+            int centerX, centerY;
+
+            if (!double.TryParse(textBox_angle.Text, out angle))
+            {
+                MessageBox.Show("Введите корректный угол");
+                return;
+            }
+
+            if (!int.TryParse(textBox_centerX.Text, out centerX) || !int.TryParse(textBox_centerY.Text, out centerY))
+            {
+                MessageBox.Show("Введите корректные координаты центра вращения");
+                return;
+            }
+
+            // Выполняем поворот изображения
+            processedImage = RotateImage(sourceImage, angle, centerX, centerY);
+
+            // Отображаем обработанное изображение
+            imageBox2.Image = processedImage;
+        }
+
+        private Image<Bgr, byte> RotateImage(Image<Bgr, byte> image, double angle, int centerX, int centerY)
+        {
+            // Создаем пустое изображение для повернутого изображения
+            Image<Bgr, byte> rotatedImage = new Image<Bgr, byte>(image.Width, image.Height);
+
+            // Переводим угол в радианы
+            double radians = angle * Math.PI / 180.0;
+
+            // Инвертируем угол для обратного поворота
+            double invRadians = -radians;
+
+            // Проходим по каждому пикселю в повернутом изображении
+            for (int y = 0; y < rotatedImage.Height; y++)
+            {
+                for (int x = 0; x < rotatedImage.Width; x++)
+                {
+                    // Вычисляем координаты в исходном изображении для текущего пикселя в повернутом изображении
+                    double srcX = (x - centerX) * Math.Cos(invRadians) - (y - centerY) * Math.Sin(invRadians) + centerX;
+                    double srcY = (x - centerX) * Math.Sin(invRadians) + (y - centerY) * Math.Cos(invRadians) + centerY;
+
+                    // Получаем координаты базовых точек
+                    int baseX = (int)Math.Floor(srcX);
+                    int baseY = (int)Math.Floor(srcY);
+
+                    // Получаем относительные расстояния от базовой точки до целевой точки
+                    double ratioX = srcX - baseX;
+                    double ratioY = srcY - baseY;
+
+                    // Учитываем выход за границы исходного изображения
+                    if (baseX < 0 || baseY < 0 || baseX >= image.Width - 1 || baseY >= image.Height - 1)
+                        continue;
+
+                    // Получаем значения цветов изображения в базовых точках
+                    Bgr c1 = image[baseY, baseX];
+                    Bgr c2 = image[baseY, baseX + 1];
+                    Bgr c3 = image[baseY + 1, baseX];
+                    Bgr c4 = image[baseY + 1, baseX + 1];
+
+                    // Применяем билинейную интерполяцию
+                    Bgr interpolatedColor = BilinearInterpolation(c1, c2, c3, c4, ratioX, ratioY);
+
+                    // Устанавливаем цвет пикселя в повернутом изображении
+                    rotatedImage[y, x] = interpolatedColor;
+                }
+            }
+
+            return rotatedImage;
+        }
+
+        private Bgr BilinearInterpolation(Bgr c1, Bgr c2, Bgr c3, Bgr c4, double ratioX, double ratioY)
+        {
+            // Применяем формулу билинейной интерполяции для каждого канала цвета
+            double blue = (1 - ratioX) * (1 - ratioY) * c1.Blue + ratioX * (1 - ratioY) * c2.Blue +
+                          (1 - ratioX) * ratioY * c3.Blue + ratioX * ratioY * c4.Blue;
+            double green = (1 - ratioX) * (1 - ratioY) * c1.Green + ratioX * (1 - ratioY) * c2.Green +
+                           (1 - ratioX) * ratioY * c3.Green + ratioX * ratioY * c4.Green;
+            double red = (1 - ratioX) * (1 - ratioY) * c1.Red + ratioX * (1 - ratioY) * c2.Red +
+                         (1 - ratioX) * ratioY * c3.Red + ratioX * ratioY * c4.Red;
+
+            // Возвращаем результирующий цвет
+            return new Bgr(blue, green, red);
+        }
+
+        //4
+
+        private void button_mirror_Click(object sender, EventArgs e)
+        {
+            if (sourceImage != null)
+            {
+                processedImage = sourceImage.Clone(); // создаем копию исходного изображения
+
+                // Отражение по горизонтали
+                for (int y = 0; y < processedImage.Height; y++)
+                {
+                    for (int x = 0; x < processedImage.Width / 2; x++)
+                    {
+                        var temp = processedImage.Data[y, x, 0];
+                        processedImage.Data[y, x, 0] = processedImage.Data[y, processedImage.Width - 1 - x, 0];
+                        processedImage.Data[y, processedImage.Width - 1 - x, 0] = temp;
+
+                        temp = processedImage.Data[y, x, 1];
+                        processedImage.Data[y, x, 1] = processedImage.Data[y, processedImage.Width - 1 - x, 1];
+                        processedImage.Data[y, processedImage.Width - 1 - x, 1] = temp;
+
+                        temp = processedImage.Data[y, x, 2];
+                        processedImage.Data[y, x, 2] = processedImage.Data[y, processedImage.Width - 1 - x, 2];
+                        processedImage.Data[y, processedImage.Width - 1 - x, 2] = temp;
+                    }
+                }
+
+                // Обновляем imageBox2 с отраженным изображением
+                imageBox2.Image = processedImage;
+            }
+        }
+
+
+        //6
+
+        private void button_projection_Click(object sender, EventArgs e)
+        {
+            if (srcPoints == null || srcPoints.Length != 4)
+            {
+                MessageBox.Show("Выберите четыре точки для проекции на плоскость.");
+                return;
+            }
+
+            // Определение точек на плоскости
+            var destPoints = new PointF[]
+            {
+        new PointF(0, 0),
+        new PointF(sourceImage.Width - 1, 0),
+        new PointF(sourceImage.Width - 1, sourceImage.Height - 1),
+        new PointF(0, sourceImage.Height - 1)
+            };
+
+            // Получение матрицы гомографической проекции
+            var homographyMatrix = CvInvoke.GetPerspectiveTransform(srcPoints, destPoints);
+
+            // Проекция изображения
+            var destImage = new Image<Bgr, byte>(sourceImage.Size);
+            CvInvoke.WarpPerspective(sourceImage, destImage, homographyMatrix, destImage.Size);
+
+            // Отображение результата во втором ImageBox
+            imageBox2.Image = destImage;
+        }
+
+        private void imageBox1_MouseClick(object sender, MouseEventArgs e)
+        {
+            int x = (int)(e.Location.X / imageBox1.ZoomScale);
+            int y = (int)(e.Location.Y / imageBox1.ZoomScale);
+
+            // Рисование круга на изображении для обозначения точки
+            Point center = new Point(x, y);
+            int radius = 2;
+            int thickness = 2;
+            var color = new Bgr(Color.Blue).MCvScalar;
+            CvInvoke.Circle(sourceImage, center, radius, color, thickness);
+
+            // Добавление точки к массиву srcPoints
+            if (srcPoints == null)
+                srcPoints = new PointF[4];
+
+            srcPoints[pointsSelected] = new PointF(x, y);
+            pointsSelected++;
+
+            if (pointsSelected == 4)
+            {
+                // Если выбраны все 4 точки, выполняем проекцию
+                button_projection_Click(sender, e);
+                pointsSelected = 0;
+            }
+        }
+
+    }
+}
