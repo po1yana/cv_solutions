b362ef48b8588e2cae34efdef61d79c6da2a7da1
Lapin Egor <104688678+po1yana@users.noreply.github.com>
2024-06-14 01:39:00 +0300

    commit lab4

---
+++ lab4_minimized/lab4/Form1.Designer.cs
@@ -0,0 +1,189 @@
+﻿namespace lab4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.imageBox1 = new Emgu.CV.UI.ImageBox();
+            this.imageBox2 = new Emgu.CV.UI.ImageBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button_remove_noise = new System.Windows.Forms.Button();
+            this.button_detect_regions = new System.Windows.Forms.Button();
+            this.button_find_contours = new System.Windows.Forms.Button();
+            this.button_find_primitives = new System.Windows.Forms.Button();
+            this.button_find_quadrilaterals = new System.Windows.Forms.Button();
+            this.numericUpDownThreshold = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDownMinArea = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownThreshold)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownMinArea)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // imageBox1
+            // 
+            this.imageBox1.Location = new System.Drawing.Point(166, 12);
+            this.imageBox1.Name = "imageBox1";
+            this.imageBox1.Size = new System.Drawing.Size(297, 272);
+            this.imageBox1.TabIndex = 2;
+            this.imageBox1.TabStop = false;
+            // 
+            // imageBox2
+            // 
+            this.imageBox2.Location = new System.Drawing.Point(469, 12);
+            this.imageBox2.Name = "imageBox2";
+            this.imageBox2.Size = new System.Drawing.Size(319, 272);
+            this.imageBox2.TabIndex = 2;
+            this.imageBox2.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(43, 18);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "open";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button_open_image_Click);
+            // 
+            // button_remove_noise
+            // 
+            this.button_remove_noise.Location = new System.Drawing.Point(34, 76);
+            this.button_remove_noise.Name = "button_remove_noise";
+            this.button_remove_noise.Size = new System.Drawing.Size(98, 23);
+            this.button_remove_noise.TabIndex = 4;
+            this.button_remove_noise.Text = "remove noise";
+            this.button_remove_noise.UseVisualStyleBackColor = true;
+            this.button_remove_noise.Click += new System.EventHandler(this.button_remove_noise_Click);
+            // 
+            // button_detect_regions
+            // 
+            this.button_detect_regions.Location = new System.Drawing.Point(34, 105);
+            this.button_detect_regions.Name = "button_detect_regions";
+            this.button_detect_regions.Size = new System.Drawing.Size(98, 23);
+            this.button_detect_regions.TabIndex = 5;
+            this.button_detect_regions.Text = "detect regions";
+            this.button_detect_regions.UseVisualStyleBackColor = true;
+            this.button_detect_regions.Click += new System.EventHandler(this.button_detect_regions_Click);
+            // 
+            // button_find_contours
+            // 
+            this.button_find_contours.Location = new System.Drawing.Point(43, 173);
+            this.button_find_contours.Name = "button_find_contours";
+            this.button_find_contours.Size = new System.Drawing.Size(75, 23);
+            this.button_find_contours.TabIndex = 6;
+            this.button_find_contours.Text = "contours";
+            this.button_find_contours.UseVisualStyleBackColor = true;
+            this.button_find_contours.Click += new System.EventHandler(this.button_find_contours_Click);
+            // 
+            // button_find_primitives
+            // 
+            this.button_find_primitives.Location = new System.Drawing.Point(22, 222);
+            this.button_find_primitives.Name = "button_find_primitives";
+            this.button_find_primitives.Size = new System.Drawing.Size(122, 23);
+            this.button_find_primitives.TabIndex = 7;
+            this.button_find_primitives.Text = "primitives triangles";
+            this.button_find_primitives.UseVisualStyleBackColor = true;
+            this.button_find_primitives.Click += new System.EventHandler(this.button_find_primitives_Click);
+            // 
+            // button_find_quadrilaterals
+            // 
+            this.button_find_quadrilaterals.Location = new System.Drawing.Point(22, 251);
+            this.button_find_quadrilaterals.Name = "button_find_quadrilaterals";
+            this.button_find_quadrilaterals.Size = new System.Drawing.Size(122, 23);
+            this.button_find_quadrilaterals.TabIndex = 8;
+            this.button_find_quadrilaterals.Text = "primitives rectangles";
+            this.button_find_quadrilaterals.UseVisualStyleBackColor = true;
+            this.button_find_quadrilaterals.Click += new System.EventHandler(this.button_find_quadrilaterals_Click);
+            // 
+            // numericUpDownThreshold
+            // 
+            this.numericUpDownThreshold.Location = new System.Drawing.Point(43, 289);
+            this.numericUpDownThreshold.Maximum = new decimal(new int[] {
+            255,
+            0,
+            0,
+            0});
+            this.numericUpDownThreshold.Name = "numericUpDownThreshold";
+            this.numericUpDownThreshold.Size = new System.Drawing.Size(75, 20);
+            this.numericUpDownThreshold.TabIndex = 9;
+            this.numericUpDownThreshold.ValueChanged += new System.EventHandler(this.numericUpDownThreshold_ValueChanged);
+            // 
+            // numericUpDownMinArea
+            // 
+            this.numericUpDownMinArea.Location = new System.Drawing.Point(43, 315);
+            this.numericUpDownMinArea.Maximum = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+            this.numericUpDownMinArea.Name = "numericUpDownMinArea";
+            this.numericUpDownMinArea.Size = new System.Drawing.Size(75, 20);
+            this.numericUpDownMinArea.TabIndex = 10;
+            this.numericUpDownMinArea.ValueChanged += new System.EventHandler(this.numericUpDownMinArea_ValueChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.numericUpDownMinArea);
+            this.Controls.Add(this.numericUpDownThreshold);
+            this.Controls.Add(this.button_find_quadrilaterals);
+            this.Controls.Add(this.button_find_primitives);
+            this.Controls.Add(this.button_find_contours);
+            this.Controls.Add(this.button_detect_regions);
+            this.Controls.Add(this.button_remove_noise);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.imageBox2);
+            this.Controls.Add(this.imageBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownThreshold)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownMinArea)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Emgu.CV.UI.ImageBox imageBox1;
+        private Emgu.CV.UI.ImageBox imageBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button_remove_noise;
+        private System.Windows.Forms.Button button_detect_regions;
+        private System.Windows.Forms.Button button_find_contours;
+        private System.Windows.Forms.Button button_find_primitives;
+        private System.Windows.Forms.Button button_find_quadrilaterals;
+        private System.Windows.Forms.NumericUpDown numericUpDownThreshold;
+        private System.Windows.Forms.NumericUpDown numericUpDownMinArea;
+    }
+}
+
---
+++ lab4_minimized/lab4/Form1.cs
@@ -0,0 +1,203 @@
+﻿using Emgu.CV.Structure;
+using Emgu.CV;
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+using Emgu.CV.CvEnum;
+using Emgu.CV.Util;
+
+namespace lab4
+{
+    public partial class Form1 : Form
+    {
+        private Image<Bgr, byte> sourceImage;
+        private Image<Bgr, byte> processedImage;
+        private Image<Bgr, byte> regionsImage;
+        private double thresholdValue = 80.0;
+        private double minContourArea = 99.0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button_open_image_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            var result = openFileDialog.ShowDialog(); // открытие диалога выбора файла
+            if (result == DialogResult.OK) // открытие выбранного файла
+            {
+                string fileName = openFileDialog.FileName;
+                sourceImage = new Image<Bgr, byte>(fileName);
+
+                imageBox1.Image = sourceImage;
+                imageBox1.SizeMode = PictureBoxSizeMode.Zoom;
+
+            }
+        }
+
+        //1,2
+        private void button_remove_noise_Click(object sender, EventArgs e)
+        {
+            processedImage = sourceImage.Clone(); // Клонируем исходное изображение для обработки
+            imageBox2.Image = processedImage;
+            if (sourceImage != null)
+            {
+                var grayImage = sourceImage.Convert<Gray, byte>();
+                int kernelSize = 5; // радиус размытия
+                var bluredImage = grayImage.SmoothGaussian(kernelSize);
+
+                processedImage = bluredImage.Convert<Bgr, byte>();
+                imageBox2.Image = processedImage;
+                imageBox2.SizeMode = PictureBoxSizeMode.Zoom;
+            }
+        }
+
+        private void button_detect_regions_Click(object sender, EventArgs e)
+        {
+            if (processedImage != null)
+            {
+                var grayImage = processedImage.Convert<Gray, byte>();
+                var threshold = new Gray(thresholdValue); // пороговое значение
+                var color = new Gray(255); // этим цветом будут закрашены пиксели, имеющие значение > threshold
+                var binarizedImage = grayImage.ThresholdBinary(threshold, color);
+
+                regionsImage = binarizedImage.Convert<Bgr, byte>();
+                imageBox2.Image = regionsImage;
+                imageBox2.SizeMode = PictureBoxSizeMode.Zoom;
+            }
+
+        }
+
+        private void button_find_contours_Click(object sender, EventArgs e)
+        {
+            if (regionsImage != null) // Используем сохраненное изображение с обнаруженными областями
+            {
+                VectorOfVectorOfPoint contours = new VectorOfVectorOfPoint();
+                CvInvoke.FindContours(regionsImage.Convert<Gray, byte>(), contours, null, RetrType.List, ChainApproxMethod.ChainApproxSimple);
+
+                var contoursImage = sourceImage.CopyBlank(); // Создаем "пустую" копию исходного изображения
+                for (int i = 0; i < contours.Size; i++)
+                {
+                    var points = contours[i].ToArray();
+                    contoursImage.Draw(points, new Bgr(Color.GreenYellow), 2); // Отрисовываем контуры
+                }
+
+                processedImage = contoursImage;
+                imageBox2.Image = processedImage;
+                imageBox2.SizeMode = PictureBoxSizeMode.Zoom;
+            }
+        }
+
+        //3
+        private void button_find_primitives_Click(object sender, EventArgs e)
+        {
+            if (regionsImage != null) // Используем сохраненное изображение с обнаруженными областями
+            {
+                VectorOfVectorOfPoint contours = new VectorOfVectorOfPoint();
+                CvInvoke.FindContours(regionsImage.Convert<Gray, byte>(), contours, null, RetrType.List, ChainApproxMethod.ChainApproxSimple);
+
+                var primitivesImage = sourceImage.CopyBlank(); // Создаем "пустую" копию исходного изображения
+
+                //double minArea = 256.0; // Минимально допустимая площадь для контуров
+
+                int triangleCount = 0;
+
+                for (int i = 0; i < contours.Size; i++)
+                {
+                    var approxContour = new VectorOfPoint();
+                    CvInvoke.ApproxPolyDP(contours[i], approxContour, CvInvoke.ArcLength(contours[i], true) * 0.05, true);
+
+                    if (CvInvoke.ContourArea(approxContour, false) > minContourArea) // Проверка на площадь контура
+                    {
+                        if (approxContour.Size == 3) // Если контур содержит 3 точки, то это треугольник
+                        {
+                            var points = approxContour.ToArray();
+                            primitivesImage.Draw(new Triangle2DF(points[0], points[1], points[2]), new Bgr(Color.GreenYellow), 2);
+                            triangleCount++;
+                        }
+                        //else // В противном случае просто рисуем аппроксимированный контур
+                        //{
+                        //    primitivesImage.Draw(approxContour.ToArray(), new Bgr(Color.Red), 2);
+                        //}
+                    }
+                }
+                string text = $"Triangles: {triangleCount}";
+                Point textLocation = new Point(10, 30); // Позиция текста чуть ниже
+                CvInvoke.PutText(primitivesImage, text, textLocation, FontFace.HersheyComplex, 0.7, new MCvScalar(255, 255, 255), 2);
+
+                processedImage = primitivesImage; // Сохраняем результат в обработанное изображение
+                imageBox2.Image = processedImage; // Отображаем его в imageBox2
+            }
+        }
+
+        private bool isRectangle(Point[] points)
+        {
+            int delta = 10; // максимальное отклонение от прямого угла
+            LineSegment2D[] edges = PointCollection.PolyLine(points, true);
+            for (int i = 0; i < edges.Length; i++) // обход всех ребер контура
+            {
+                double angle = Math.Abs(edges[(i + 1) % edges.Length].GetExteriorAngleDegree(edges[i]));
+                if (angle < 90 - delta || angle > 90 + delta) // если угол непрямой
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        private void button_find_quadrilaterals_Click(object sender, EventArgs e)
+        {
+            if (regionsImage != null) // Используем сохраненное изображение с обнаруженными областями
+            {
+                VectorOfVectorOfPoint contours = new VectorOfVectorOfPoint();
+                CvInvoke.FindContours(regionsImage.Convert<Gray, byte>(), contours, null, RetrType.List, ChainApproxMethod.ChainApproxSimple);
+
+                var primitivesImage = sourceImage.CopyBlank(); // Создаем "пустую" копию исходного изображения
+
+                //double minArea = 100.0; // Минимально допустимая площадь для контуров
+
+                int rectCount = 0;
+
+                for (int i = 0; i < contours.Size; i++)
+                {
+                    var approxContour = new VectorOfPoint();
+                    CvInvoke.ApproxPolyDP(contours[i], approxContour, CvInvoke.ArcLength(contours[i], true) * 0.05, true);
+
+                    if (CvInvoke.ContourArea(approxContour, false) > minContourArea) // Проверка на площадь контура
+                    {
+                        if (approxContour.Size == 4 && isRectangle(approxContour.ToArray())) // Если контур содержит 4 точки и является прямоугольником
+                        {
+                            primitivesImage.Draw(CvInvoke.MinAreaRect(approxContour), new Bgr(Color.GreenYellow), 2); // Отрисовываем прямоугольник
+                            rectCount ++;
+                        }
+                        //else // В противном случае просто рисуем аппроксимированный контур
+                        //{
+                        //    primitivesImage.Draw(approxContour.ToArray(), new Bgr(Color.Red), 2);
+                        //}
+                    }
+                }
+
+                string text = $"Rectangles: {rectCount}";
+                Point textLocation = new Point(10, 30); // Позиция текста чуть ниже
+                CvInvoke.PutText(primitivesImage, text, textLocation, FontFace.HersheyComplex, 0.7, new MCvScalar(255, 255, 255), 2);
+
+                processedImage = primitivesImage; // Сохраняем результат в обработанное изображение
+                imageBox2.Image = processedImage; // Отображаем его в imageBox2
+            }
+        }
+
+        //4
+
+        private void numericUpDownThreshold_ValueChanged(object sender, EventArgs e)
+        {
+            thresholdValue = (double)numericUpDownThreshold.Value;
+        }
+
+        private void numericUpDownMinArea_ValueChanged(object sender, EventArgs e)
+        {
+            minContourArea = (double)numericUpDownMinArea.Value;
+        }
+
+
+    }
+}
